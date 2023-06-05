73981db771ec0df51666b15d2fe7e305a8a6612c
vanessa <vanessaduquerubio@gmail.com>
2023-06-05 09:15:38 +0200

    add no funciona

--- js/function.js
+++ js/function.js
@@ -3,30 +3,14 @@
 //capturar elementos del dom
 
 const sectionTareas = document.querySelector('#tareas')
+const addTitulo = document.querySelector('#floatingInputGrid')
+const addPrioridad = document.querySelector('#floatingSelectGrid')
+const addButton = document.querySelector('#buttonadd')
+
+console.log(addTitulo.value)
 
 //funcion que pinte una tarea en el dom
-
-/* 
 function printOneTarea(pTarea, pDom) {
-    const divUrgencia = document.createElement('div');
-    divUrgencia.classList.add('card')
-    const divPintado = document.createElement('div')
-    divPintado.classList.add('card-body');
-    const pParrafo = document.createElement('p')
-    pParrafo.textContent = '${ pTarea.titulo }'
-    const button = document.createElement('button')
-    button.classList.add('btn, btn-outline-danger')
-    button.textContent = 'Eliminar'
-    divPintado.append(p, button)
-    divUrgencia.appendChild(divPintado)
-    sectionTareas.appendChild(divUrgencia)
-    pDom.appendChild(sectionTareas)
-    console.log(divUrgencia)
-}
-
-// imprimir todas las tareas que hay en el array de datos;/*  */
-
-function printOneTarea(pTarea, pDom, pList) {
     const divUrgencia = document.createElement('div');
     divUrgencia.classList.add('card')
     const divPintado = document.createElement('div')
@@ -45,13 +29,45 @@ function printOneTarea(pTarea, pDom, pList) {
 }
 
 function printAllTareas(pList, pDom) {
-    pList.forEach(tarea => printOneTarea(tarea, pDom, tareas));
+    pList.forEach(tarea => printOneTarea(tarea, pDom));
 }
 
 printAllTareas(tareas, sectionTareas);
 
-//añadir estilo por prioridad
+//fin printTareas
+
+// asegurarnos que los campos no estan vacios
+/* function comprobarForm(pForm) {
+    return pForm.titulo.value !== "" && pForm.prioridad.value !== ""
+}
+ */
+//crear funcion para comprobar que no está repetida
+function saveTarea(pList, pTarea) {
+    let duplicado = pList.findIndex(tarea => tarea.titulo === pTarea.titulo)
+    if (duplicado === -1) {
+        pList.push(pTarea);
+        return 'usuario guardado';
+    }
+    return 'usuario duplicado'
+}
+//inicion addTareas, conseguir  el valor de addTitulo y de addPrioridad y enviarlo con un evento de clik en addButton
+
+let id = 0;
 
 
+function addTarea(event) {
+    event.preventDefault();
+
+    const newTarea = {
+        idTarea: id,
+        titulo: event.target.titulo.value,
+        prioridad: event.target.prioridad.value,
+    }
+    console.log(newTarea)
+    printOneTarea(newTarea, sectionTareas)
+    let guardado = saveTarea(tareas, newTarea)
+    return guardado
+}
 
 
+addButton.addEventListener('click', addTarea);
